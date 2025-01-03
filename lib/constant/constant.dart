4212d7df201362c97b28b1d8f59e56ccad4ead64
rosfandy <brosfandy@gmail.com>
2025-01-03 15:45:37 +0700

    update force clean architecture project

---
+++ lib/constant/constant.dart
@@ -0,0 +1,3 @@
+const String apiServiceUrl = 'http://192.168.1.28:8002/rest/v1';
+const String apiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsb2JqeWFwZ3p3ZHFpdnBiZGZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ0Mzg0MjAsImV4cCI6MjA1MDAxNDQyMH0.-zJT9WeECDFzbhtyUUXEWHFh_LzX7fVEM5vMjB1lyEI';
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:new_mobile/features/doctors/presentation/pages/doctors_page.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: DoctorListScreen(),
+    );
+  }
+}
